cd5ff155c1f80afd3be2811b42505a87c8b437aa
Aurore Maureaud <aurore.aqua@gmail.com>
2024-06-18 18:46:41 +0200

    implement cross-sector probabilities per year

---
+++ functions/get_probas.R
@@ -0,0 +1,14 @@
+# quick demonstration: https://docs.google.com/spreadsheets/d/1D3g9lWAuQZLnhTfk_nwV8CDNRBPPFW6jx4r3iPAB-lI/edit?gid=0#gid=0
+
+get_at_least_probas <- function(n, k, names_of_n){
+  
+  combi <- combn(n,k)
+  
+  if(k == 1){
+    
+  } else (k == 2){
+    
+  } else (k == 3){
+    
+  }
+}
